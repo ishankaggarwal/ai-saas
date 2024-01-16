135d64c6820a9ea6a5bcc3af82764c6dbb71e5a5
ishankaggarwal <ishank193aggarwal@gmail.com>
2024-01-15 18:56:58 -0800

    Fixed sidebar height

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:flex-col md:w-72 md:fixed z-80 bg-gray-800">
-        <div>
+        <div className="h-full">
           <Sidebar />
         </div>
       </div>
