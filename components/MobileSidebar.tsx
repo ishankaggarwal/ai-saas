8c73db5df4797938aa5839e142e2010b879bd063
ishankaggarwal <ishank193aggarwal@gmail.com>
2024-01-15 18:49:31 -0800

    Fixed: Hydration errors

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -4,8 +4,19 @@ import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/Sidebar";
+import { useEffect, useState } from "react";
 
 function MobileSidebar() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
