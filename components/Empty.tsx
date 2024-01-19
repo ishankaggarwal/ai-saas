eeb4f6abb83ce10556ee1c70bd08a5f819c9bd18
ishankaggarwal <ishank193aggarwal@gmail.com>
2024-01-18 19:53:18 -0800

    Fix:Empty.tsx

--- components/Empty.tsx
+++ components/Empty.tsx
@@ -8,7 +8,7 @@ function Empty({ label }: Props) {
   return (
     <div className="p-20 flex flex-col items-center justify-center">
       <div className="h-72 w-72 relative">
-        <Image alt="Empty" fill src={"/empty.png"} />
+        <Image alt="Empty" width={400} height={400} src={"/empty.png"} />
       </div>
       <p className="text-center text-sm font-light">{label}</p>
     </div>
