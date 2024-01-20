e925158384513d1d9a3f97ef9a11fd736e0c71c5
ishankaggarwal <ishank193aggarwal@gmail.com>
2024-01-20 13:17:28 -0800

    Added LandingNavbar component

---
+++ components/LandingHero.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useAuth } from "@clerk/nextjs";
+
+function LandingHero() {
+  const { isSignedIn } = useAuth();
+
+  return (
+    <div className="text-white font-bold py-36 text-center space-y-5">
+      <div className="text-4xl sm:txt-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+        <h1>The Best AI Tool for</h1>
+      </div>
+    </div>
+  );
+}
+
+export default LandingHero;
