b06fd4b5972bc9068cd6b8d4816ca81e3073e620
ishankaggarwal <ishank193aggarwal@gmail.com>
2024-01-28 12:35:19 -0800

    Fixed loading issues

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,14 @@
 import LandingHero from "@/components/LandingHero";
 import LandingNavbar from "@/components/LandingNavbar";
+import { auth } from "@clerk/nextjs";
 
 function Landing() {
+  const { userId }: { userId: string | null } = auth();
+
   return (
     <div className="h-full">
-      <LandingNavbar />
-      <LandingHero />
+      <LandingNavbar userId={userId} />
+      <LandingHero userId={userId} />
     </div>
   );
 }
--- components/LandingHero.tsx
+++ components/LandingHero.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { useAuth } from "@clerk/nextjs";
 import Link from "next/link";
 import TypewriterComponent from "typewriter-effect";
 import { Button } from "./ui/button";
 
-function LandingHero() {
-  const { isSignedIn } = useAuth();
-
+function LandingHero({ userId }: { userId: string | null }) {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:txt-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
@@ -32,7 +29,7 @@ function LandingHero() {
         Create content using AI 10x faster
       </div>
       <div>
-        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+        <Link href={userId ? "/dashboard" : "/sign-up"}>
           <Button
             variant="premium"
             className="md:text-lg p-4 md:p-6 rounded-full font-semibold"
